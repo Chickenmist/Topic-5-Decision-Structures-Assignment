83f58c9a58e6811da444794280298bf3418e3072
Chickenmist <wpimlatt@gmail.com>
2024-02-26 12:57:17 -0500

    Made menu, finished parts 1 and 2, part 3 not started

--- Topic 5 (Decision Structures) Assignment/Program.cs
+++ Topic 5 (Decision Structures) Assignment/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        //Wilson
+        
         static void Main(string[] args)
         {
             Menu();
@@ -10,20 +12,49 @@
 
         public static void Menu()
         {
-            BankMachine();
+            string selection = "";
+
+            Console.WriteLine("-------WELCOME TO THE MENU-------");
+            Console.WriteLine("Please select a program:");
+            Console.WriteLine("Bank");
+            Console.WriteLine("Parking");
+            Console.WriteLine("Hurricane");
+
+            while (selection == "")
+            {
+                selection = Console.ReadLine().ToLower();
+
+                if (selection == "bank")
+                {
+                    Console.WriteLine("");
+                    BankMachine();
+                }
+                else if (selection == "parking")
+                {
+                    Console.WriteLine("");
+                    ParkingGarage();
+                }
+                else if (selection == "hurricane")
+                {
+                    Console.WriteLine("");
+                    Hurricane();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection");
+                    Console.Write("Select a program: ");
+                    Console.WriteLine("");
+                }
+            }
+
         }
 
         //Part 1
         public static void BankMachine()
         {
+            Random random = new Random();
             string selectionCheck = "";
-            double balance = 0, transactionFee = 0.75, depositWithdrawlPaymentAmount = 0, updatedBalance;
-            int firstAccess = 0; //0 means it hasn't been accessed, 1 means it has. This is for tracking the bank balance
-
-            if (firstAccess == 0)
-            {
-                balance = 150.00;
-            }
+            double balance = random.Next(100, 151), transactionFee = 0.75, depositWithdrawlPaymentAmount = 0;
 
             Console.WriteLine("-------WELCOME TO THE BANK OF BLORB-------");
             Console.WriteLine("");
@@ -50,12 +81,13 @@
                 if (depositWithdrawlPaymentAmount == 0)
                 {
                     Console.WriteLine("Invalid deposit");
+                    Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                    Console.WriteLine($"Balance: {balance.ToString("C")}");
                 }
                 else
                 {
                     balance += depositWithdrawlPaymentAmount;
 
-                    Console.WriteLine("");
                     Console.WriteLine($"Deposit: {depositWithdrawlPaymentAmount.ToString("C")}");
                     Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
                     Console.WriteLine($"Balance: {balance.ToString("C")}");
@@ -64,28 +96,99 @@
             }
             else if (selectionCheck == "2" || selectionCheck == "withdrawl")
             {
+                Console.WriteLine("");
+                Console.Write("Input withdrawl amount: ");
+                double.TryParse(Console.ReadLine(), out depositWithdrawlPaymentAmount);
+                Console.WriteLine("");
 
+                if (depositWithdrawlPaymentAmount == 0 || depositWithdrawlPaymentAmount > balance)
+                {
+                    Console.WriteLine("Invalid withdrawl");
+                    Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                    Console.WriteLine($"Balance: {balance.ToString("C")}");
+                }
+                else
+                {
+                    balance -= depositWithdrawlPaymentAmount;
+
+                    Console.WriteLine($"Withdrawl: {depositWithdrawlPaymentAmount.ToString("C")}");
+                    Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                    Console.WriteLine($"Balance: {balance.ToString("C")}");
+                }
             }
             else if (selectionCheck == "3" || selectionCheck == "bill payment")
             {
+                Console.WriteLine("");
+                Console.Write("Input payment amount: ");
+                double.TryParse(Console.ReadLine(), out depositWithdrawlPaymentAmount);
+                Console.WriteLine("");
 
+                if (depositWithdrawlPaymentAmount > balance || depositWithdrawlPaymentAmount == 0)
+                {
+                    Console.WriteLine("Invalid payment amount");
+                    Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                    Console.WriteLine($"Balance: {balance.ToString("C")}");
+                }
+                else
+                {
+                    balance -= depositWithdrawlPaymentAmount;
+
+                    Console.WriteLine($"Payment Amount: {depositWithdrawlPaymentAmount.ToString("C")}");
+                    Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                    Console.WriteLine($"Balance: {balance.ToString("C")}");
+                }
             }
             else if (selectionCheck == "4" || selectionCheck == "account balance update")
             {
-
+                Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                Console.WriteLine($"Updated Balance: {balance.ToString("C")}");
             }
             else
             {
-                Menu();
+                Console.WriteLine($"Transaction Fee: {transactionFee.ToString("C")}");
+                Console.WriteLine($"Balance: {balance.ToString("C")}");
             }
 
+            Console.WriteLine("");
+            Console.WriteLine("-------THANK YOU FOR CHOOSING THE BANK OF BLORB-------");
+            Console.WriteLine("");
+            Menu();
         }
         //
 
         //Part 2
         public static void ParkingGarage()
         {
+            double hours, minutes, payment;
+            Console.WriteLine("-------SAM'S PARKING GARAGE-------");
+            Console.WriteLine("");
+            Console.WriteLine("How many minutes were you parked for?");
 
+            double.TryParse(Console.ReadLine(), out minutes);
+
+
+            hours = minutes/60;
+
+            if (Math.Ceiling(hours) == 1)
+            {
+                payment = 4;
+            }
+            else
+            {
+                payment = 4 + 2 * Math.Ceiling(hours);
+
+                if (payment > 20)
+                {
+                payment = 20;
+                }
+            }
+
+            Console.WriteLine($"Hours Parked: {Math.Ceiling(hours)}");
+            Console.WriteLine($"Cost: {payment.ToString("C")}");
+            Console.WriteLine("");
+            Console.WriteLine("-------THANK YOU FOR YOUR STAY-------");
+            Console.WriteLine("");
+            Menu();
         }
         //
 
